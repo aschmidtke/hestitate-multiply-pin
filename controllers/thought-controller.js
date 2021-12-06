ab6e066db05d15ce751e5880b5956e7b9f7878f0
aschmidtke <87831418+aschmidtke@users.noreply.github.com>
2021-12-05 19:44:18 -0500

    controller outlines

---
+++ controllers/thought-controller.js
@@ -0,0 +1,31 @@
+const { User, Thought } = require('../models');
+
+const thoughtController = {
+    getAllThoughts(req, res) {
+
+    },
+
+    getThoughtById({ params }, res) {
+
+    },
+
+    createThought({ body }, res) {
+
+    },
+
+    updateThoughtById({ params, body }, res) {
+
+    },
+
+    deleteThought({ params }, res) {
+
+    },
+
+    createReaction({ params }, res) {
+
+    },
+
+    deleteReaction({ params }, res) {
+        
+    }
+}
---
+++ controllers/user-controller.js
@@ -0,0 +1,33 @@
+const { User } = require('../models');
+
+const userController = {
+    getAllUsers(req, res) {
+
+    },
+
+    getUserById({ params }, res) {
+
+    },
+
+    createUser({ body }, res) {
+
+    },
+
+    updateUser({ params, body}, res) {
+
+    },
+
+    deleteUser({ params }, res) {
+
+    },
+
+    addFriend() {
+
+    },
+
+    deleteFriend() {
+
+    }
+};
+
+module.exports = userController;
